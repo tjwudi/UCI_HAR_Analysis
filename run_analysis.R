3c569d0365e2f74d1e3032c9319e7ff17734ff92
John Wu <webmaster@leapoahead.com>
2015-09-25 13:39:49 -0700

    Initial Commit

---
+++ run_analysis.R
@@ -0,0 +1,6 @@
+######################################################
+# run_analysis.R
+#
+# Course Project of Getting and Cleaning Data Course
+# Author: John Wu <webmaster@leapoahead.com>
+######################################################
