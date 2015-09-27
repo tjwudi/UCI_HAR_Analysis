4c42cd0c1a02bfd5abfb7eafc4499581e7f7a83a
tjwudi <webmaster@leapoahead.com>
2015-09-26 18:39:34 -0700

    Update

--- run_analysis.R
+++ run_analysis.R
@@ -41,11 +41,13 @@ uciData.test.y  <- uci.helpers.readAsDataTable(file.path(uci.datasetPath, 'y_tes
 uciData.test.sub <- uci.helpers.readAsDataTable(file.path(uci.datasetPath, 'subject_test.txt'))
 uciData.features <- uci.helpers.readAsDataTable(file.path(uci.datasetPath, 'features.txt'))
 
+print("Processing")
+
 # Merge train and test data
-names(uciData.train.y) <- "activityId"
-names(uciData.test.y) <- "activityId"
-names(uciData.train.sub) <- "subject"
-names(uciData.test.sub) <- "subject"
+setnames(uciData.train.y, names(uciData.train.y), c("activityId"))
+setnames(uciData.test.y, names(uciData.test.y), c("activityId"))
+setnames(uciData.train.sub, names(uciData.train.sub), c("subject"))
+setnames(uciData.test.sub, names(uciData.test.sub), c("subject"))
 uciData.train <- cbind(uciData.train.x, uciData.train.y, uciData.train.sub)
 uciData.test  <- cbind(uciData.test.x, uciData.test.y, uciData.test.sub)
 uciData.all <- rbind(uciData.train, uciData.test)
@@ -59,15 +61,32 @@ remove(uciData.train.sub)
 remove(uciData.test.sub)
 
 # Select measurements on mean and std
-names(uciData.features) <- c('featureId', 'featureName')
+setnames(uciData.features, names(uciData.features), c('featureId', 'featureName'))
 featuresOnMeanOrStd <- uciData.features[grepl("mean\\(\\)|std\\(\\)", uciData.features$featureName),]
 featureIdsOnMeanOrStd <- c(paste('V', featuresOnMeanOrStd$featureId, sep=''), 'activityId', 'subject')
 uciData.all <- uciData.all[, featureIdsOnMeanOrStd, with = F]
 
 # Set descriptive activity names
 uciData.activityNames <- uci.helpers.readAsDataTable(file.path(uci.datasetPath, 'activity_labels.txt'))
-names(uciData.activityNames) <- c("activityId", "activityName")
+setnames(uciData.activityNames, names(uciData.activityNames), c("activityId", "activityName"))
 setnames(uciData.all, paste0('V',featuresOnMeanOrStd$featureId), as.character(featuresOnMeanOrStd$featureName))
 
 # Write tidy dataset
-write.table(uciData.all, file="result.txt", row.names = F)
+write.table(uciData.all, file="dataset.txt", row.names = F)
+
+# Generate dataset required by step 5
+uciData.all$activityId <- factor(uciData.all$activityId)
+uciData.all$subject <- factor(uciData.all$subject)
+uciData.grouped <- split(uciData.all, list(uciData.all$activityId, uciData.all$subject))
+uciData.summaryMean <- list()
+for (item in uciData.grouped) {
+  item$subject <- as.numeric(item$subject)
+  item$activityId <- as.numeric(item$activityId)
+  uciData.summaryMean <- c(uciData.summaryMean, colMeans(item))
+}
+uciData.summaryMean <- matrix(
+  unlist(uciData.summaryMean),
+  ncol = nrow(featuresOnMeanOrStd) + 2,
+  byrow = T)
+colnames(uciData.summaryMean) <- colnames(uciData.grouped$`1.1`)
+write.table(uciData.summaryMean, 'summary_mean.txt', row.names = F)
